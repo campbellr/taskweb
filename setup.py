81f787a8f1e1fc64d4dfe536b55fce9a2e982c26
Ryan Campbell <campbellr@gmail.com>
2011-12-17 18:23:04 -0700

    work on setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+        name='taskweb',
+        version='0.1',
+        description="Django-based web front-end to taskwarrior",
+        long_description=open('README').read(),
+        author="Ryan Campbell",
+        author_email="campbellr@gmail.com",
+        url="http://github.com/campbellr/taskweb",
+        )
