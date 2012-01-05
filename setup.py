5d198efb3eff5bf66dc0baa1a39bec3c2e009712
Ryan Campbell <campbellr@gmail.com>
2012-01-04 19:46:43 -0700

    Make setup.py actually work

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+requirements = [
+        'taskw == 0.1.8-campbellr2',
+        'Django >= 1.3',
+        'django-tables2',
+    ]
+
+dependency_links = [
+  'https://github.com/downloads/campbellr/taskw/taskw-0.1.8-campbellr2.tar.gz',
+        ]
+with open('README') as f:
+    long_description = f.read()
 
 setup(
         name='taskweb',
         version='0.1',
+        install_requires=requirements,
+        dependency_links=dependency_links,
         description="Django-based web front-end to taskwarrior",
-        long_description=open('README').read(),
+        long_description=long_description,
         author="Ryan Campbell",
         author_email="campbellr@gmail.com",
         url="http://github.com/campbellr/taskweb",
+        packages=find_packages(),
+        zip_safe=False,
+        include_package_data=True,
         )
