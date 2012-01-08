b850936c90c577433991bfd1a4411d9da41499df
Ryan Campbell <campbellr@gmail.com>
2012-01-07 17:53:43 -0700

    Add some simple tests

--- django_task/task/tests.py
+++ django_task/task/tests.py
@@ -1,16 +1,46 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestViews(TestCase):
+    def test_pending_tasks(self):
+        response = self.client.get('/pending/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_completed_tasks(self):
+        response = self.client.get('/completed/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_task_GET(self):
+        self._create_user_and_login()
+        response = self.client.get('/add/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_tasks_GET_no_login(self):
+        response = self.client.get('/add/')
+        # should redirect ot login screen
+        self.assertRedirects(response, '/accounts/login/?next=/add/')
+
+    def test_add_tasks_POST(self):
+        pass
+
+    def test_add_tasks_POST_no_login(self):
+        pass
+
+    def test_taskdb_GET(self):
+        self._create_user_and_login()
+        response = self.client.get('/taskdb/pending.data')
+        self.assertEqual(response.status_code, 200)
+
+    def test_taskdb_POST(self):
+        self._create_user_and_login()
+
+    def test_taskdb_PUT(self):
+        self._create_user_and_login()
+
+    def _create_user_and_login(self):
+        user = User.objects.create_user('foo', 'foo@test.com', 'bar')
+        user.save()
+        self.assertTrue(user.check_password('bar'))
+        success = self.client.login(username='foo', password='bar')
+        self.assertTrue(success)
