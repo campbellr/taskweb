23224091dd68331575404690498d10a69113902d
Ryan Campbell <campbellr@gmail.com>
2012-03-11 22:27:58 -0600

    comment out debug-toolbar

--- django_task/settings.py
+++ django_task/settings.py
@@ -111,7 +111,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.transaction.TransactionMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    #'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'django_task.urls'
@@ -143,7 +143,7 @@ INSTALLED_APPS = (
     'djblets.util',
     'django.contrib.admin',
     'task',
-    'debug_toolbar',
+    #'debug_toolbar',
 )
 
 # A sample logging configuration. The only tangible logging
